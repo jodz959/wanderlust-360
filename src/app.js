7a974be5fb62da9cad6aabe701ab7539913e60eb
Jody <smile.jody@gmail.com>
2018-11-10 18:52:38 -0500

    add routes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
+const homeRouter = require('./routes/home-route');
+app.use('/home', homeRouter);
 app.get('/', function(req, res){
     res.render('dashboard', {title_page: "Wanderlust 360", trip_name: "NYC"});
 });
