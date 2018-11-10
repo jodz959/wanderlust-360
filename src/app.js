65c183d41934d5dfd1fcad8aaa4fbb4d606f7c9a
StickyKnees <jsl9985@gmail.com>
2018-11-10 15:55:41 -0500

    Created server and init layout

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 //express app goes here
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+    res.render('layout', {title: "wander"});
+});
+
+app.listen(3000);
