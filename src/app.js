dbe483b566dc1ae2cfb93b2ce4e5ac8abe3545c8
StickyKnees <jsl9985@gmail.com>
2018-11-11 03:17:54 -0500

    Made tabs and cards

--- src/app.js
+++ src/app.js
@@ -11,15 +11,24 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: false}));
 
 const homeRouter = require('./routes/home-route');
+<<<<<<< Updated upstream
 const loginRouter = require('./routes/login-route');
 const signupRouter = require('./routes/signup-route');
 
+=======
+app.use(express.static(__dirname + "/public"));
+>>>>>>> Stashed changes
 app.use('/home', homeRouter);
 app.use('/login', loginRouter);
 app.use('/sign-up', signupRouter);
 
 app.get('/', function(req, res){
+<<<<<<< Updated upstream
    res.render('dashboard', {title_page: "Wanderlust 360", trip_name: "NYC"});
+=======
+    res.render('dashboard', {title_page: "Wanderlust 360",
+     trip_name: "China WinterTrip", summary: "Winter Vacation with my Best Buds!"});
+>>>>>>> Stashed changes
 });
 
 app.listen(3000);
